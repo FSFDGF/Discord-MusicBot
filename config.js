a436fddbafd5d9daca25af2889d7b49f383c51a1
FSFDGF <130705363+FSFDGF@users.noreply.github.com>
2023-10-15 15:28:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	token: process.env.token || "MTE2Mjg4NDM5Njg3Mzk0OTI4NQ.GM7Agw.b8MH5ypsPkHkPM7ePNgUXU5AJ3FjJOf78Tzz2I", //- Bot's Token
+	clientId: process.env.clientId || "1162884396873949285", //- ID of the bot
+	clientSecret: process.env.clientSecret || "p3sJ1qzzf7UDpH6HvnkRDavHKmceRxrP", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
